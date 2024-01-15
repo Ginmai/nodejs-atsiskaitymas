5fc0cc634b6c7babd9af1202be30333f57ebea54
Ginmai <gintare.kort@gmail.com>
2024-01-15 22:04:36 +0200

    missing autosave issue fix

--- src/controllers/ticket.js
+++ src/controllers/ticket.js
@@ -1,25 +1,5 @@
 import TicketModel from "../models/ticket.js";
 
-const GET_ALL_TICKETS = async (rec, res) => {
-  try {
-    const tickets = await TicketModel.find();
-    return res.status(200).json({ tickets: tickets });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ ticket: "something went wrong" });
-  }
-};
-
-const GET_TICKET_BY_ID = async (req, res) => {
-  try {
-    const ticket = await TicketModel.findById(req.params.id);
-    return res.status(200).json({ ticket: ticket });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ resource: "something went wrong" });
-  }
-};
-
 const INSERT_TICKET = async (req, res) => {
   try {
     const ticket = new TicketModel({
@@ -41,35 +21,4 @@ const INSERT_TICKET = async (req, res) => {
   }
 };
 
-const UPDATE_TICKET = async (req, res) => {
-  try {
-    const ticket = await TicketModel.findOneAndUpdate(
-      { _id: req.params.id },
-      { ...req.body },
-      { new: true }
-    );
-
-    return res.status(200).json({ ticket: ticket });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ ticket: "something went wrong" });
-  }
-};
-
-const DELETE_TICKET_BY_ID = async (req, res) => {
-  try {
-    const ticket = await TicketModel.findOneAndDelete(req.params.id);
-    return res.status(200).json({ ticket: ticket });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ ticket: "something went wrong" });
-  }
-};
-
-export {
-  GET_ALL_TICKETS,
-  GET_TICKET_BY_ID,
-  INSERT_TICKET,
-  UPDATE_TICKET,
-  DELETE_TICKET_BY_ID,
-};
+export { INSERT_TICKET };
--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,24 +1,10 @@
 import express from "express";
-import {
-  GET_ALL_TICKETS,
-  GET_TICKET_BY_ID,
-  INSERT_TICKET,
-  UPDATE_TICKET,
-  DELETE_TICKET_BY_ID,
-} from "../controllers/ticket.js";
+import { INSERT_TICKET } from "../controllers/ticket.js";
 
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/tickets", GET_ALL_TICKETS);
-
-router.get("/tickets/:id", GET_TICKET_BY_ID);
-
-router.post("/tickets", INSERT_TICKET);
-
-router.put("/tickets/:id", UPDATE_TICKET);
-
-router.delete("/tickets/:id", DELETE_TICKET_BY_ID);
+router.post("/insert_ticket", auth, INSERT_TICKET);
 
 export default router;
